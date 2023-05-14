ea3ecbb98cc8890016b7e9a621845f3818585a06
Radzhan Aziskhanov <revel.2015@mail.ru>
2023-05-14 11:42:56 +0600

    Добавьте файлы проекта.

---
+++ ItStepQuiz/Controllers/Room/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItStepQuiz.Controllers.Room;
+
+public class RoomController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> FindRoom()
+    {
+        return View();
+    }
+}
